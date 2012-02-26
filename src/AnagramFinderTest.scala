76cfa1615147a698bd8d5308fa96e60bee572c8d
Gabriel Klappenbach <gabriel.klappenbach@gmail.com>
2012-02-26 19:52:19 +0100

    introduced must matchers in anagramFinderTest

--- src/AnagramFinderTest.scala
+++ src/AnagramFinderTest.scala
@@ -1,21 +1,28 @@
-class AnagramFinderTest extends org.scalatest.FunSuite {
-  test("simple two word anagram is found") {
-    val anagrams: Set[AnagramGroup] = new AnagramFinder().findAnagrams(List(Word("lamp"), Word("palm")))
-    assert(anagrams.size == 1 && anagrams.exists(_.groupSize == 2), "didn't find the anagrams: " + anagrams)
-  }
-  
-  test("duplicated word in input is not duplicated in anagrams output"){
-    val anagrams: Set[AnagramGroup] = new AnagramFinder().findAnagrams(List(Word("lamp"), Word("palm"), Word("palm")))
-    assert(anagrams.size == 1 && anagrams.exists(_.groupSize == 2), "didn't find the anagrams: " + anagrams)
-  }
+import org.scalatest.matchers.MustMatchers
+
+class AnagramFinderTest extends org.scalatest.FunSuite with MustMatchers {
+  val one = 1
+  val two = 2
 
   test("finds two groups with anagrams"){
     val anagrams: Set[AnagramGroup] = new AnagramFinder().findAnagrams(List(Word("lamp"), Word("palm"), Word("lame"), Word("meal")))
-    assert(anagrams.size == 2 && anagrams.forall(_.groupSize == 2), "didn't find the anagrams: " + anagrams)
+    anagrams must have size two
+    assertAllGroupsHaveSizeTwo(anagrams)
+  }
+
+  test("duplicated word in input is not duplicated in anagrams output"){
+    val anagrams: Set[AnagramGroup] = new AnagramFinder().findAnagrams(List(Word("lamp"), Word("palm"), Word("palm")))
+    anagrams must have size one
+    assertAllGroupsHaveSizeTwo(anagrams)
   }
 
   test("words with no anagrams are excluded from output"){
     val anagrams: Set[AnagramGroup] = new AnagramFinder().findAnagrams(List(Word("lamp"), Word("palm"), Word("lame")))
-    assert(anagrams.size == 1 && anagrams.exists(_.groupSize == 2), "didn't find the anagrams: " + anagrams)
+    anagrams must have size one
+    assertAllGroupsHaveSizeTwo(anagrams)
   }
+
+  def assertAllGroupsHaveSizeTwo(anagrams: Set[AnagramGroup]) {
+      assert(anagrams.forall(_.groupSize == 2), "didn't find the anagrams: " + anagrams)
+    }
 }
